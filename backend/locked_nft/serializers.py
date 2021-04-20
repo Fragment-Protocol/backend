006430dab3d40de40f8de1272b56448c373aaa58
serm-fragprot <serm@fragmentprotocol.com>
2021-04-20 14:42:10 +0300

    Add serializers

--- backend/locked_nft/serializers.py
+++ backend/locked_nft/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from backend.locked_nft.models import LockedNFT
+from backend.locked_nft.models import LockedNFT, BEP20
+
+
+class BEP20Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = BEP20
+        fields = '__all__'
 
 
 class LockedNFTSerializer(serializers.ModelSerializer):
+    bep20 = BEP20Serializer()
+
     class Meta:
         model = LockedNFT
         fields = '__all__'
