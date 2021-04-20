a1fc819e61c9b671d89d510a199926ff83cb2521
serm-fragprot <serm@fragmentprotocol.com>
2021-04-20 23:14:57 +0300

    Update checker

--- checker.py
+++ checker.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
@@ -18,15 +19,18 @@ rpc.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 
 def process():
-    addresses = LockedNFT.objects.all()
+    addresses = LockedNFT.objects.filter(bep20__isnull=False)
+    print(f'Check {len(addresses)} addresses')
     for address in addresses:
-        contract = rpc.eth.contract(address=rpc.toChecksumAddress('0x2170ed0880ac9a755fd29b2688956bd959f933f8'),
-                                    abi=bep20_abi)
+        contract = rpc.eth.contract(address=rpc.toChecksumAddress(address.bep20.tokenAddress), abi=bep20_abi)
         current_balance = contract.functions.balanceOf(contract.address).call()
         total = contract.functions.totalSupply().call()
         address.bep20.current_balance = current_balance
         address.bep20.total = total
+        address.save()
 
 
 if __name__ == '__main__':
-    process()
+    while True:
+        process()
+        time.sleep(60)
